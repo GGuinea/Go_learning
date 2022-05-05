1a0c32b4a3c84e0882b00fca1a5c74128ca0b0e8
Gguinea <Gguinea>
2022-05-06 01:36:57 +0200

    Simple URL params handling

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func serverRequest(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "test")
+type response struct {
+	StandardDeviation int   `json:"stddev"`
+	NumList           []int `json:"data"`
+}
+
+func getRandom(length int) int {
+	return length
+}
+
+func serverRequest(w http.ResponseWriter, r *http.Request) {
+	var simpleList = []response{
+		{StandardDeviation: 43, NumList: []int{1, 2, 3, 4}},
+		{StandardDeviation: 23, NumList: []int{2, 2, 2, 2}},
+	}
+
+	requestParam, ok := r.URL.Query()["requests"]
+	if !ok || len(requestParam[0]) < 1 {
+		log.Println("Problem with URL param 'rquests'")
+		return
+	}
+
+	lengthParam, ok := r.URL.Query()["length"]
+	if !ok || len(lengthParam[0]) < 1 {
+		log.Println("Problem with URL 'length' param")
+		return
+	}
+
+	requestNumber := requestParam[0]
+	length := lengthParam[0]
+	log.Println("is: " + string(requestNumber))
+	log.Println("is: " + string(length))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(simpleList)
+
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", serverRequest)
-	router.Run("localhost:8080")
+
+	http.HandleFunc("/random/mean", serverRequest)
+	http.ListenAndServe(":8080", nil)
 }
