38d5264167ba93b30790a803656acbcc514a29dd
Gguinea <Gguinea>
2022-05-08 13:45:09 +0200

    Stddev unit tests

--- MathService_test.go
+++ MathService_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_WhenListIsEmptyShouldReturnZero(t *testing.T) {
+func Test_Mean_WhenListIsEmptyShouldReturnZero(t *testing.T) {
 	var xs = []int{}
 	want := 0.0
 	mean := getMean(xs)
@@ -13,7 +13,7 @@ func Test_WhenListIsEmptyShouldReturnZero(t *testing.T) {
 	}
 }
 
-func Test_WhenOneAndTwoShouldReturnOneAndHalf(t *testing.T) {
+func Test_Mean_WhenOneAndTwoShouldReturnOneAndHalf(t *testing.T) {
 	var xs = []int{1, 2}
 	want := 1.5
 	mean := getMean(xs)
@@ -22,11 +22,38 @@ func Test_WhenOneAndTwoShouldReturnOneAndHalf(t *testing.T) {
 	}
 }
 
-func Test_WhenDoubleZeroShouldReturnZero(t *testing.T) {
+func Test_Mean_WhenDoubleZeroShouldReturnZero(t *testing.T) {
 	var xs = []int{0, 0}
 	want := 0.0
 	mean := getMean(xs)
 	if want != mean {
 		t.Fatalf("Should return: %f , returned: %f ", want, mean)
+	}
+}
+
+func Test_Stddev_WhenZeroShouldReturnZero(t *testing.T) {
+	var xs = []int{0}
+	want := 0.0
+	stddev := getStdDev(xs)
+	if want != stddev {
+		t.Fatalf("Should return: %f , returned: %f ", want, stddev)
+	}
+}
+
+func Test_Stddev_WhenDoubleZeroShouldReturnZero(t *testing.T) {
+	var xs = []int{0, 0}
+	want := 0.0
+	stddev := getStdDev(xs)
+	if want != stddev {
+		t.Fatalf("Should return: %f , returned: %f ", want, stddev)
+	}
+}
+
+func Test_Stddev_WhenOneAndTwoShouldReturnZeroAndHalf(t *testing.T) {
+	var xs = []int{1, 2}
+	want := 0.5
+	stddev := getStdDev(xs)
+	if want != stddev {
+		t.Fatalf("Should return: %f , returned: %f ", want, stddev)
 	}
 }
